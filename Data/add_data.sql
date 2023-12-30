1ccd7c7610ce2f8f7f2dd238a099dc1cfb7b63f8
THKhai <116781770+THKhai@users.noreply.github.com>
2023-12-30 21:01:48 +0700

    Add data for table

---
+++ Data/add_data.sql
@@ -0,0 +1,55 @@
+﻿use QuanLyPhongKhamNhaKhoa_HQT
+go
+
+BULK INSERT KhachHang
+FROM 'D:\data_KhachHang.csv'
+WITH (
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2 ); -- Nếu có header
+go
+BULK INSERT NhaSi
+FROM 'D:\data_NhaSi.csv'
+with (
+	FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2
+	)
+go
+BULK INSERT Thuoc
+FROM 'D:\data_Thuoc.csv'
+with (
+	FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2,
+	CODEPAGE = '65001'
+	)
+go
+BULK INSERT LichHen
+FROM 'D:\data_LichHen.csv'
+with (
+	FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2,
+	CODEPAGE = '65001'
+	)
+go
+
+BULK INSERT DonThuoc
+FROM 'D:\data_DonThuoc.csv'
+with (
+	FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2,
+	CODEPAGE = '65001'
+	)
+go
+BULK INSERT HoSoBenhNhan
+FROM 'D:\data_HoSoBenhNhan.csv'
+with (
+	FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2,
+	CODEPAGE = '65001'
+	)
+go
