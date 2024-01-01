5bf4aa3d8b63c718bc3cce857f2307ac5599fee7
THKhai <116781770+THKhai@users.noreply.github.com>
2024-01-01 09:19:42 +0700

    Update Program.cs

--- DoAnHEQUANTRI/DoAnHEQUANTRI/Program.cs
+++ DoAnHEQUANTRI/DoAnHEQUANTRI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DoAnHEQUANTRI.PhanHeNhaSi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace DoAnHEQUANTRI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Home());
+            Application.Run(new LoginNS());
         }
     }
 }
